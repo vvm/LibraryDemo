5bf7ed9ca25ab362d2d01fee3b5dc64d2595427b
vee <v2.cloud@gmail.com>
2017-04-01 11:45:39 +0800

    添加库工程

---
+++ StaticLibrary/StaticLibrary/StaticLibrary.h
@@ -0,0 +1,13 @@
+//
+//  StaticLibrary.h
+//  StaticLibrary
+//
+//  Created by vee on 2017/4/1.
+//  Copyright © 2017年 None. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StaticLibrary : NSObject
+
+@end
---
+++ StaticLibrary/StaticLibrary/StaticLibrary.m
@@ -0,0 +1,13 @@
+//
+//  StaticLibrary.m
+//  StaticLibrary
+//
+//  Created by vee on 2017/4/1.
+//  Copyright © 2017年 None. All rights reserved.
+//
+
+#import "StaticLibrary.h"
+
+@implementation StaticLibrary
+
+@end
