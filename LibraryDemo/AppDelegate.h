ef00a8995ef15365bb1ab21241db64d58b20b11d
vee <v2.cloud@gmail.com>
2017-04-01 11:34:41 +0800

    Initial Commit

---
+++ LibraryDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LibraryDemo
+//
+//  Created by vee on 2017/4/1.
+//  Copyright © 2017年 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LibraryDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LibraryDemo
+//
+//  Created by vee on 2017/4/1.
+//  Copyright © 2017年 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
