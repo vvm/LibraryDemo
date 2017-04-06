dfe20e5a738ca3de28af63a0decfd8f3b3e3af78
vee <v2.cloud@gmail.com>
2017-04-06 11:39:15 +0800

    动态库的符号会全部链接

---
+++ DyFrameWork/Custom3.h
@@ -0,0 +1,15 @@
+//
+//  Custom3.h
+//  LibraryDemo
+//
+//  Created by vee on 2017/4/6.
+//  Copyright © 2017年 None. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Custom3 : NSObject
+
+- (void)demo;
+
+@end
---
+++ DyFrameWork/Custom3.m
@@ -0,0 +1,23 @@
+//
+//  Custom3.m
+//  LibraryDemo
+//
+//  Created by vee on 2017/4/6.
+//  Copyright © 2017年 None. All rights reserved.
+//
+
+#import "Custom3.h"
+
+@implementation Custom3
+
++ (void)load
+{
+    printf("custom 3 is load");
+}
+
+- (void)demo
+{
+    NSLog(@"custom3 demo");
+}
+
+@end
